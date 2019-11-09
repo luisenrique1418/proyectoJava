750164c2bd67c04802e8f9c6aa0df9fb2a32889f
Alonso Martinez <jalonso.mtztios@gmail.com>
2019-11-09 16:41:55 -0600

    EnviandoCorreos

--- web/PHP/veriPage.php
+++ web/PHP/veriPage.php
@@ -1,7 +1,8 @@
 <?php 
 $type = $_POST["type"];
 $acceso = $_POST["BUTTON"];
-
+$conexion = mysqli_connect('localhost','root','','contactoitl');
+if($conexion){
 switch($acceso){
     case "Alumno":
         $NoCtrl = $_POST["NoCtrl"];
@@ -12,10 +13,16 @@ switch($acceso){
         $carrera = $_POST["carrera"];
         $semestre = $_POST["semestre"];
         $correo = $_POST["email"];
-        $SQLQuery = 
-        "INSERT INTO `alumno`(`NoCtrl`, `NombreAlumno`, `EdadAlumno`, `SexoAlumno`, `CarreraAlumno`, `Semestre`, `Email`, `Password`, `Habilitado`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9])";
+        $query = mysqli_query($conexion,"INSERT INTO `alumno`(`NoCtrl`, `NombreAlumno`, `EdadAlumno`, `SexoAlumno`, `CarreraAlumno`, `Semestre`, `Email`, `Password`) VALUES ('$NoCtrl','$Nombre',32,'$sexo','$carrera',33,'$correo','$Contraseña')");
+        if($query){
+            $headers = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+            $contenido = "Porfavor verifique su cuenta <br> presione en el siguiente enlace <br> <a href='http://localhost/proyectoJava/web/PHP/habilitar.php/?NoControl=$NoCtrl'>presione aquí parfa verificar su cuenta</a> <br> Una vez en el sitio tendra que iniciar sesion.";
+            mail($correo,"VERIFICACION DE CUENTA",$contenido,$headers);
+        }
         break;
     case "Egresado":
+        $table = 'egresado';
         $NoCtrl = $_POST["NoCtrl"];
         $Nombre = $_POST["nombre"];
         $Contraseña = $_POST["password"];
@@ -28,6 +35,7 @@ switch($acceso){
     break;
     case "Profesor":
         $NoCtrl = $_POST["NoCtrl"];
+        $table = 'profesor';
         $Nombre = $_POST["nombre"];
         $Contraseña = $_POST["password"];
         $edad = $_POST["edad"];
@@ -37,4 +45,34 @@ switch($acceso){
         $correo = $_POST["email"];
     break;
 }
+}else{
+    ?>
+    <script type="text/javascript" charset="utf-8">
+    function lohicimos() {
+        alert(
+            "No se llevo a cabo la conexion"
+        );
+    }
+    lohicimos();
+    </script>
+    <?php
+}
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div margin=20% > 
+        <h1>
+            Hemos enviado un correo a tu direccion email para verificar tu cuenta, porfavor hazlo.
+            <h1>
+    <div>
+</body>
+</html>
